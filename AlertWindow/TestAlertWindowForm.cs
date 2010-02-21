c5962e488e650b8dcb939d3744a86d12b82d0a91
Koichi-Kobayashi <koichi.code@gmail.com>
2010-02-21 09:31:56 +0000

    .NET Framework 3.5 → 2.0 に変更

--- AlertWindow/TestAlertWindowForm.cs
+++ AlertWindow/TestAlertWindowForm.cs
@@ -2,9 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 
 public partial class TestAlertWindowForm : Form
